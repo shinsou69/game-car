032a1dbe855be50d403bd0af908ac65ba79812e0
koshihara <107600389+shinsou69@users.noreply.github.com>
2022-06-16 13:26:28 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1655360727,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/fram-sheet0.png",
+		"images/car-sheet0.png",
+		"images/tree-sheet0.png",
+		"images/blast-sheet0.png",
+		"images/blast-sheet1.png",
+		"images/blast-sheet2.png",
+		"images/star-sheet0.png",
+		"images/gameover-sheet0.png",
+		"images/button-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
